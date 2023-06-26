c50bf27c2da3ad37073aa0b13637f6e06362311d
tofof <github@tofof.com>
2023-06-26 13:35:50 -0500

    refactor to make editing by lab easier

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,24 @@
-/*
-  Arduino pink noise generation adapted from Mark Tillotson's adaptation of Stenzel
-  Works for Uno/Pro Mini/Mega, using pin 11 or 10 depending on board (the one driven by timer2 output A)
-  Noncommercial use only.
- */
-
 #include <Arduino.h>
 #include "firtables.h"    // 12 tap FIR filter using 2 lookups on last 12 random bits
 #include <debounce.h>
 
-// define the OCR2A timer output pin, depends on which board - only support ATmega328 and ATmega1280/2560
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-#  define AUDIO_OUTPUT_PIN 10
-#else
-#  define AUDIO_OUTPUT_PIN 11
-#endif
+/* Using timings of 1000/500/5000, the current sequence will be:
+                  0 s    sequence start button pushed
+[offset]          1.0 s  pre-sound imaging starts
+[image duration]  1.5 s  pre-sound imaging stops
+[offset]          2.5 s  sound starts
+[calculated]      4.75 s imaging starts (centered on middle of sound playback)
+[image duration]  5.25 s imaging stops
+                  7.5 s  sound stops
+[offset]          8.5 s  post-sound imaging starts
+[image duration]  9.0 s  post-sound imaging stops*/
 
-// On the Uno, the output pin is 11, but the high-frequency components of
-// the signal bleed onto 10 and 12 so avoid using these.
-#define SEQUENCE_BUTTON_PIN 9
-#define TEST_BUTTON_PIN 8
-#define TTL_OUTPUT_PIN 7
-#define SOUND_LED_PIN 6
-#define TRANSISTOR_PIN 5
-
-// EDIT THESE VALUES TO ADJUST SEQUENCE TIMING
+// EDIT THESE VALUES to adjust timings on existing sequence.
 #define OFFSET 1000           // ms between steps in sequence
 #define IMAGE_DURATION 500    // ms duration of TTL signal for imaging
 #define SOUND_DURATION 5000   // ms duration of sound to play
-/* If the above settings are 1000/500/5000, the sequence will be:
-      0 s     sequence start button pushed
-      [offset]
-      1.0 s  pre-sound imaging starts
-      [image duration]
-      1.5 s  pre-sound imaging stops
-      [offset]
-      2.5 s  sound starts
-      [calculated time to center imaging during middle of sound playback]
-      4.75 s imaging starts
-      [image duration]
-      5.25 s imaging stops
-      7.5 s  sound stops
-      [offset]
-      8.5 s  post-sound imaging starts
-      [image duration]
-      9.0 s  post-sound imaging stops
-*/
 
+// EDIT THIS SECTION to define sequence itself
 unsigned long currentMillis = 0;
 unsigned long imageStart[3] = {0};
 unsigned long imageStop[3] = {0};
@@ -53,74 +26,6 @@ unsigned long soundStart = 0;
 unsigned long soundStop = 0;
 bool sendingTTL = false;
 bool playingSound = false;
-
-volatile byte outsampl = 0x80;  // cache last sample for ISR
-volatile byte phase = 0;  // oscillates between 0 <-> 1 for each interrupt
-volatile int error = 0;   // trick to reduce quantization noise
-byte bitrevtab[0x40];  // caches bit reversal at cost of 64 bytes RAM
-
-// Stenzel "new shade of pink" algorithm state
-long lfsr = 0x5EED41F5;  // feedback RNG
-int inc = 0xCCC;         // incrementing bit per octave
-int dec = 0xCCC;         // decrementing bit per octave
-int accum = 0;           // accumulator
-int counter = 0xAAA;     // used to chose bit position
-
-byte bitrev (byte a) { // reverse a 6 bit value 
-  byte r = 0;
-  for (byte i = 0; i < 6; i++) {
-    r <<= 1;
-    r |= a & 1;
-    a >>= 1;
-  }
-  return r;
-}
-
-void build_revtab() {
-  for (byte i = 0; i < 0x40; i++) {
-    bitrevtab [i] = bitrev (i);
-  }
-}
-
-int stenzel_pink_sample () {
-  int bit = lfsr < 0 ? 0xFFF : 0x000;  // step the RNG, keeping latest bit as a mask in variable 'bit'
-  lfsr += lfsr;
-  if (bit) lfsr ^= 0x46000001L;
-    
-  counter += 1;
-  counter &= 0xFFF;                    // step counter
-  int bitmask = counter & -counter;    // get lowest bit
-  bitmask = bitrevtab [bitmask & 0x3F];// reverse using 2 lookups
-  bitmask <<= 6;
-  bitmask |= bitrevtab [bitmask >> 6];
-  
-  dec &= ~bitmask;         // clear the dec bit at position given by bitmask
-  dec |= inc & bitmask;    // copy the bit at that position from inc to dec (cancelling the effect of inc)
-  inc ^= bit & bitmask;    // depending on latest random bit, perhaps flip the inc bit at that position
-  accum += inc - dec;      // difference calculates the linear interpolations for all 12 generators simultaneously
-  int result = accum;
-
-  int twelve_bits = ((int) lfsr) & 0xFFF;   // extract last 12 random bits generated and filter to correct spectral
-  result += highfirtab [twelve_bits >> 6];  // power density at the higher end of spectrum
-  result += lowfirtab  [twelve_bits & 0x3F];
-  
-  return result;
-}
-
-// output the latest sample, regenerate new sample every two interrupts (since it takes longer than 16us)
-// Thus PWM is 62.5 kHz, actual sample rate 31.25 kSPS, so a hardware anti-aliasing filter with cutoff below
-// 15kHz is recommended
-// Since only 8 bit samples are output, the quantization noise probably pollutes the higher frequencies
-ISR (TIMER2_OVF_vect) {
-  if (phase == 0) {
-    OCR2A = outsampl + 0x80;    // PWM output, offset is 50% duty cycle for 8 bit timer2
-    int samp = stenzel_pink_sample () + error;
-    outsampl = samp >> 8;
-    error = samp - (outsampl<<8);  // recalculate error
-  }
-  phase = 1 - phase;
-}
-
 static void sequenceHandler(uint8_t btnId, uint8_t btnState) {
   if (btnState == BTN_PRESSED) {
     Serial.println("Pressed sequence button");
@@ -192,6 +97,14 @@ static void testHandler(uint8_t btnId, uint8_t btnState) {
   }
 }
 
+// On the Uno, the relevant timer pin for audio output pin is 11, but the
+// high-frequency components of the signal bleed onto 10 and 12. Avoid them.
+#define SEQUENCE_BUTTON_PIN 9
+#define TEST_BUTTON_PIN 8
+#define TTL_OUTPUT_PIN 7
+#define SOUND_LED_PIN 6
+#define TRANSISTOR_PIN 5
+
 // Define button with a unique id (0) and handler function.
 // (The ids are so one handler function can tell different buttons apart if necessary.)
 static Button seqButton(0, sequenceHandler);
@@ -203,6 +116,85 @@ static void pollButtons() {
   seqButton.update(digitalRead(SEQUENCE_BUTTON_PIN));
   testButton.update(digitalRead(TEST_BUTTON_PIN));
 }
+
+/*
+Arduino pink noise generation adapted from Mark Tillotson's adaptation of Stenzel
+Works for Uno/Pro Mini/Mega, using pin 11 or 10 depending on board (the one driven by timer2 output A)
+Noncommercial use only.
+*/
+    // define the OCR2A timer output pin, depends on which board - only support ATmega328 and ATmega1280/2560
+    #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    #  define AUDIO_OUTPUT_PIN 10
+    #else
+    #  define AUDIO_OUTPUT_PIN 11
+    #endif
+    volatile byte outsampl = 0x80;  // cache last sample for ISR
+    volatile byte phase = 0;  // oscillates between 0 <-> 1 for each interrupt
+    volatile int error = 0;   // trick to reduce quantization noise
+    byte bitrevtab[0x40];  // caches bit reversal at cost of 64 bytes RAM
+
+    // Stenzel "new shade of pink" algorithm state
+    long lfsr = 0x5EED41F5;  // feedback RNG
+    int inc = 0xCCC;         // incrementing bit per octave
+    int dec = 0xCCC;         // decrementing bit per octave
+    int accum = 0;           // accumulator
+    int counter = 0xAAA;     // used to chose bit position
+
+    byte bitrev (byte a) { // reverse a 6 bit value 
+      byte r = 0;
+      for (byte i = 0; i < 6; i++) {
+        r <<= 1;
+        r |= a & 1;
+        a >>= 1;
+      }
+      return r;
+    }
+
+    void build_revtab() {
+      for (byte i = 0; i < 0x40; i++) {
+        bitrevtab [i] = bitrev (i);
+      }
+    }
+
+    int stenzel_pink_sample () {
+      int bit = lfsr < 0 ? 0xFFF : 0x000;  // step the RNG, keeping latest bit as a mask in variable 'bit'
+      lfsr += lfsr;
+      if (bit) lfsr ^= 0x46000001L;
+        
+      counter += 1;
+      counter &= 0xFFF;                    // step counter
+      int bitmask = counter & -counter;    // get lowest bit
+      bitmask = bitrevtab [bitmask & 0x3F];// reverse using 2 lookups
+      bitmask <<= 6;
+      bitmask |= bitrevtab [bitmask >> 6];
+      
+      dec &= ~bitmask;         // clear the dec bit at position given by bitmask
+      dec |= inc & bitmask;    // copy the bit at that position from inc to dec (cancelling the effect of inc)
+      inc ^= bit & bitmask;    // depending on latest random bit, perhaps flip the inc bit at that position
+      accum += inc - dec;      // difference calculates the linear interpolations for all 12 generators simultaneously
+      int result = accum;
+
+      int twelve_bits = ((int) lfsr) & 0xFFF;   // extract last 12 random bits generated and filter to correct spectral
+      result += highfirtab [twelve_bits >> 6];  // power density at the higher end of spectrum
+      result += lowfirtab  [twelve_bits & 0x3F];
+      
+      return result;
+    }
+
+    // output the latest sample, regenerate new sample every two interrupts (since it takes longer than 16us)
+    // Thus PWM is 62.5 kHz, actual sample rate 31.25 kSPS, so a hardware anti-aliasing filter with cutoff below
+    // 15kHz is recommended
+    // Since only 8 bit samples are output, the quantization noise probably pollutes the higher frequencies
+    ISR (TIMER2_OVF_vect) {
+      if (phase == 0) {
+        OCR2A = outsampl + 0x80;    // PWM output, offset is 50% duty cycle for 8 bit timer2
+        int samp = stenzel_pink_sample () + error;
+        outsampl = samp >> 8;
+        error = samp - (outsampl<<8);  // recalculate error
+      }
+      phase = 1 - phase;
+    }
+/* Arduino pink noise generation ends. */
 
 void setup() {
   Serial.begin(115200);
@@ -236,7 +228,6 @@ void loop() { // nothing here for ongoing pink noise, all driven by ISR
       silenceSound();
     }
   }
-
 
   delay(10);
 }
